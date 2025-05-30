9357739d8bf2caa3a143743cf999e5dbaa2f3b5a
Dallan Borgheresi <dallanr@gmail.com>
2025-05-30 18:13:59 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import streamlit as st
 from agente import criar_agente
+import os
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+
+
 
 st.set_page_config(page_title="Agente Inteligente para Notas Fiscais - Jan/2024", layout="wide")
-
+st.image("images/logo.jpg", width=100)
 st.title("🤖 Agente Inteligente para Notas Fiscais - Jan/2024")
 
-st.markdown("💬 *Faça uma pergunta sobre o arquivo CSV (pressione Enter para enviar)*")
+st.markdown("""
+💬 *Faça uma pergunta sobre o arquivo CSV (pressione Enter para enviar)*
+
+**Exemplos de perguntas:**
+- 🧾 Qual o valor total das notas fiscais?
+- 📍 Quais estados mais emitiram notas?
+- 👤 Quais os principais destinatários?
+""")
+
 
 try:
     agente = criar_agente("202401_NFs_Cabecalho.csv")
